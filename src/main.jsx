879d3408f4379e464390d873bfe4175f2b948822
abacodata-dev <abacodata@gmail.com>
2026-04-29 08:51:39 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WineApp from './WineApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WineApp />
+  </React.StrictMode>
+)
 
